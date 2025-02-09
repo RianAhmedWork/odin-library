dfcf47bb5a8be2c4833f44b116c41983e3331834
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-02-09 01:50:45 -0400

    add function to display all books on the webpage

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
+
+function displayBooksOnPage() {
+    for (book of myLibrary) {
+        const bookCard = document.createElement("div");
+        bookCard.classList = "book-card";
+        const bookTitle = document.createElement("div");
+        bookTitle.classList = "book-title";
+        bookTitle.textContent = book.title;
+        const bookAuthor = document.createElement("div");
+        bookAuthor.classList = "book-author";
+        bookAuthor.textContent = book.author;
+        const bookPages = document.createElement("div");
+        bookPages.classList = "book-pages";
+        bookPages.textContent = book.pages;
+        const bookRead = document.createElement("div");
+        bookRead.classList = "book-class"
+        bookRead.textContent = book.read;
+        bookCard.append(bookTitle);
+        bookCard.append(bookAuthor);
+        bookCard.append(bookPages);
+        bookCard.append(bookRead);
+        document.getElementById("main").append(bookCard);
+    }
+}
